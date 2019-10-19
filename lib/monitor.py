358918937e77dbaa7bed671ffc10bcd3cbe0afac
David Füchslin <david.fuchslin@gmail.com>
2019-10-19 20:52:03 +0200

    configure graphite and prometheus

--- lib/monitor.py
+++ lib/monitor.py
@@ -1,5 +1,6 @@
-from prometheus_client import start_http_server, Summary
-import time
+from prometheus_client import Counter, CollectorRegistry, start_http_server
+from prometheus_client.bridge.graphite import GraphiteBridge
+import time, random
 
 class Monitor:
 
@@ -7,8 +8,8 @@ class Monitor:
     self.config = config
     self.graphite_host = self.get_config_or_default('graphite_host', 'localhost')
     self.graphite_port = self.get_config_or_default('graphite_port', 2003)
-    self.metric_path = self.get_config_or_default('metric_path', 'test.energy.monitor.path')
-    self.report_interval = self.get_config_or_default('report_interval', 60)
+    self.metric_prefix = self.get_config_or_default('metric_prefix', 'test.energy.monitor.prefix')
+    self.report_interval = self.get_config_or_default('report_interval', 10)
     self.prometheus_port = self.get_config_or_default('prometheus_port', 9101)
 
   def get_config_or_default(self, config_param, default):
@@ -21,16 +22,21 @@ class Monitor:
     print("Starting energy monitor:")
     print("  graphite:")
     print("    server         : {}:{}".format(self.graphite_host, self.graphite_port))
-    print("    metric path    : {}".format(self.metric_path))
+    print("    metric prefix  : {}".format(self.metric_prefix))
     print("    report interval: {:d}".format(self.report_interval))
     print("  prometheus:")
-    print("    server: localhost:{}".format(self.prometheus_port))
-    print("    path  : /metrics")
+    print("    port: {}".format(self.prometheus_port))
+    print("    path: /metrics")
 
-    start_http_server(self.prometheus_port)
+    registry = CollectorRegistry()
+    self.counter = Counter('meter_power', 'Total watt-hours consumed', registry = registry)
+    gb = GraphiteBridge((self.graphite_host, self.graphite_port), registry = registry)
+    gb.start(self.report_interval, prefix = self.metric_prefix)
+    start_http_server(self.prometheus_port, registry = registry)
 
     while True:
       time.sleep(1)
+      self.counter.inc(random.randint(0,100))
       print("tick tock")
 
   def cleanup(self):
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
   config = {}
   config['graphite_host'] = os.getenv('GRAPHITE_HOST')
   config['graphite_port'] = os.getenv('GRAPHITE_PORT')
-  config['metric_path'] = os.getenv('METRIC_PATH')
+  config['metric_prefix'] = os.getenv('METRIC_PREFIX')
   config['report_interval'] = os.getenv('REPORT_INTERVAL')
   config['prometheus_port'] = os.getenv('PROMETHEUS_PORT')
 
